b6941924b6f400f669c0d7872be616b8521bbd53
Katttanagatari <b3sh1k@gmail.com>
2025-03-14 21:07:29 +0400

    feat(main): add delete functionality to student-cart

--- js/courses-cart.js
+++ js/courses-cart.js
@@ -1,16 +1,15 @@
+const container = document.querySelector(".hero__cart-container");
+const cartContainer = document.getElementById("student-cart");
+const paymentContainer = document.getElementById("payment-methods");
+const cartOverAll = document.getElementById("cart-count");
 
-const postMethods = () => {
-    const container = document.querySelector(".hero__cart-container");
-    const cartContainer = document.getElementById("student-cart");
-    const paymentContainer = document.getElementById("payment-methods");
-    const cartOverAll = document.getElementById("cart-count");
+cartContainer.innerHTML = "";
+paymentContainer.innerHTML = "";
+let count = 0;
 
-    cartContainer.innerHTML = "";
-    paymentContainer.innerHTML = "";
-    let count = 0;
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
+const postMethods = () => {    
     if (cart.length === 0) {
         cartContainer.classList.add("cart-empty");
         cartContainer.innerHTML = `
@@ -49,19 +48,19 @@ const postMethods = () => {
                 <div class="hero__tag">
                     <div class="hero__layout hero__pay">
                         <label class="hero__option">
-                            <input type="radio" name="payment-${item.id}" style="margin: 0 8px 0 0;">
+                            <input type="checkbox" name="payment-${item.id}" value="${item.price}" style="margin: 0 8px 0 0;">
                             <span class="hero__option-title">Оплата за 1/мес</span>
                         </label>
                         <div class="hero__option-price">${item.price} ₽</div>
                     </div>
                     <div class="hero__layout hero__pay">
                         <label class="hero__option">
-                            <input type="radio" name="payment-${item.id}" style="margin: 0 8px 0 0;">
+                            <input type="checkbox" name="payment-${item.id}" value="${item.price7m}" style="margin: 0 8px 0 0;">
                             <span class="hero__option-title">Оплата за 7/мес</span>
                         </label>
                         <div class="hero__option-price">${item.price7m} ₽</div>
                     </div>
-                    <img src="https://www.svgrepo.com/show/499592/close-x.svg" alt="Закрыть" width="24" height="24" style="margin-left: 12px; cursor: pointer;" onclick="removeFromCart(${item.id})">
+                    <img src="https://www.svgrepo.com/show/499592/close-x.svg" class="hero__remove" alt="Закрыть" data-id="${item.id}">
                 </div>
             </div>
         `;
@@ -70,22 +69,83 @@ const postMethods = () => {
             <div class="hero__layout" style="width: 338px;">
                 <div class="payment__header">
                     <div class="payment__label">К оплате</div>
-                    <div class="payment__amount">6 380 ₽</div>
+                    <div class="payment__amount">0 ₽</div>
                 </div>
                 <div class="payment__details">
                     <div class="payment__courses">Курсы (${count})</div>
-                    <div style="font-size: 16px;">6 380 ₽</div>
+                    <div class="payment__amount" style="font-size: 16px;">0 ₽</div>
                 </div>
                 <div class="payment__choose">
-                    <i></i>
+                    <div class="payment__info">
+                        <span class="payment__text" data-active="true">Картой онлайн</span>
+                        <span class="payment__text">По реквезитам</span>
+                    </div>
+                    <i class="payment__indicator" id="payment-choose"></i>
                 </div>
-                <div class="main__btn" style="background-color: var(--main-bg-color); margin-top: 12px">123</div>
+                <span class="main__btn payment__btn">Перейти к оплате</span>
             </div>
         `
         cartOverAll.innerHTML = `
             <span class="hero__name">Корзина (${count})</span>
         `
     });
-}
+
+    //toggle button
+    const toggle = document.getElementById("payment-choose");
+    const toggleText = document.querySelectorAll(".payment__text");
+    toggleText.forEach((text, index) => {
+        text.addEventListener('click', () => {
+            toggleText.forEach(el => el.removeAttribute('data-active'));
+            text.setAttribute('data-active', 'true');
+            if (index === 0) {
+                toggle.classList.remove('pay-by-requisites');
+            } else {
+                toggle.classList.add('pay-by-requisites');
+                text.setAttribute('data-active', 'true');
+            }
+        });
+    });
+
+    //total sum of courses
+    const paymentAmountEl = document.querySelector('.payment__amount');
+    const paymentDetailAmountEl = document.querySelector('.payment__details div:nth-child(2)');
+    const recalcTotalSum = () => {
+        let total = 0;
+        const checkedBoxs = document.querySelectorAll('input[type="checkbox"][name^="payment-"]:checked');
+    
+        checkedBoxs.forEach(e => {
+            total += parseFloat(e.value);
+        });
+        console.log(total);
+
+        paymentAmountEl.textContent = `${total} ₽`;
+        paymentDetailAmountEl.textContent = `${total} ₽`
+    };
+
+    document.addEventListener('change', (e) => {
+        if (e.target.matches('input[type="checkbox"][name^="payment-"]')) {
+            const sameCheckboxes = document.querySelectorAll(`input[type="checkbox"][name="${e.target.name}"]`);
+            sameCheckboxes.forEach(el => {
+                if (el !== e.target) {
+                    el.checked = false;
+                }
+            });
+    
+            recalcTotalSum();
+        }
+    });
+
+    //remove from cart
+    const removeCart = document.querySelectorAll('.hero__remove');
+    removeCart.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            const id = parseInt(event.target.dataset.id); 
+            cart = cart.filter(item => item.id !== id);
+            localStorage.setItem("cart", JSON.stringify(cart));
+            event.target.closest('.hero__cart').remove();
+        });
+    });
+};
 
 document.addEventListener("DOMContentLoaded", postMethods);
+
